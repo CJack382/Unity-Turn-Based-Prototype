cc9780817b820a36cafd4ee3902a34da2aea01d4
CJack382 <carsonjackson52@gmail.com>
2025-12-24 22:33:50 -0500

    End of Day-5 and HW
    IDragHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler  are Interfaces that you should inherit (there are others, this project only needed these ones) should you wish to interact with mouse inputs in a more sophisticated manner, they allows you to drag, check if beginning to click, check if hovering, and check if no longer hovering in that order (using the methods you MUST inherit). Interesting fact, the /= operator is real, and it works exactly how you'd expect it to, pretty much divide equals. Vector3.Lerp actually stands for Linear Extrapolation, somehow I didn't know that... It takes 2 Vector locations (beginning position, endposition, time) and extrapolates from beginning position to the endposition multiplied by the speed of time, the lower you have time, the slower this movement will be.

--- Turn-Based-Prototype/Assets/_Scripts/CardMovement.cs
+++ Turn-Based-Prototype/Assets/_Scripts/CardMovement.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler 
 {
@@ -24,10 +25,12 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
     [SerializeField] private GameObject glowEffect; //Highlight image
     [SerializeField] private GameObject playArrow;
 
+    [SerializeField] private float lerpFactor = 0.1f;
+
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
-        canvas = GetComponent<Canvas>();
+        canvas = GetComponentInParent<Canvas>();
         originalScale = rectTransform.localScale;
         originalPosition = rectTransform.localPosition;
         originalRotation = rectTransform.localRotation;
@@ -43,14 +46,14 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
                 break;
             case 2:
                 HandleDragState();
-                if (!Input.GetMouseButtonDown(0)) //If player is not pressing 0
+                if (Mouse.current.leftButton.ReadValue() == 0) //If releasing the click button (***NEW INPUT SYSTEM NOT THE SAME AS VID***)
                 {
                     TransitionToState0();
                 }
                 break;
             case 3:
                 HandlePlayState();
-                if (!Input.GetMouseButtonDown(0)) //If player is not pressing 0
+                if (Mouse.current.leftButton.ReadValue() == 0)
                 {
                     TransitionToState0();
                 }
@@ -66,6 +69,81 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
         rectTransform.localScale = originalScale;
 
         glowEffect.SetActive(false);
+        playArrow.SetActive(false);
+    }
 
+    public void OnPointerEnter(PointerEventData eventData) //OnPointerEnter checks if the mouse is beginning to hover over an object, in this case it sets the state to 1, activating HandleHoverState()
+    { //Also to note, PointerEventData is merely the information that the cursor is sending to Unity
+        if (currentState == 0) //Making currentState 0 in transitiontostate0 allows us to ensure that no overlap happens where HandleHoverState() is called before the card has been reset to its standard parameters
+        {
+            originalPosition = rectTransform.localPosition;
+            originalRotation = rectTransform.localRotation;
+            originalScale = rectTransform.localScale;
+
+            currentState = 1; 
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) //OnPointerDown Checks if the player has clicked or begun to hold down the Mouse 1 button, in this case it enters state 2 and allows you to drag a card
+    {
+        if (currentState == 1)
+        {
+            currentState = 2;
+            //Finds a specific point in the rectangle (first variable), using the PointerEventData (second variable), and the camera (If Screen Space overlay, it's null, in this case the screen space scales w/screen size, so you
+            //take the camera data from PointerEventData). The final variable is the specific point you wish to find
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition);
+            originalPanelLocalPosition = rectTransform.localPosition;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (currentState == 2) 
+        {
+            Vector2 localPointerPosition;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPointerPosition))
+            {
+                rectTransform.position = Vector3.Lerp(rectTransform.position, Input.mousePosition, lerpFactor);
+
+                if(Input.mousePosition.y > cardPlay.y) //Whenever card is dragged above predetermined y position
+                {
+                    currentState = 3;
+                    playArrow.SetActive(true);
+                    rectTransform.localPosition = Vector3.Lerp(rectTransform.position, playPosition, lerpFactor);
+                }
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) //OnPointerExit occurs when the mouse cursor is no longer hovering over an object, so in this case it checks if you stop hovering over a card when in state 1, then returns it to 0
+    {
+        if (currentState == 1)
+        {
+            TransitionToState0();
+        }
+    }
+
+    private void HandleHoverState() //When hovering over a card, highlights and slight increases the size of the card
+    {
+        glowEffect.SetActive(true);
+        rectTransform.localScale = originalScale * selectScale;
+    }
+
+    private void HandleDragState()
+    {
+        //Set card rotation to 0
+        rectTransform.localRotation = Quaternion.identity;
+    }
+
+    private void HandlePlayState()
+    {
+        rectTransform.localPosition = playPosition;
+        rectTransform.localRotation = Quaternion.identity;
+        
+        if (Input.mousePosition.y < cardPlay.y)
+        {
+            currentState = 2;
+            playArrow.SetActive(false);
+        }
     }
 }
